324072991e08f69cf76acc551fdc067c11793f3c
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-02-04 10:16:07 +0800

    minor rename

--- api-messages/app.py
+++ api-messages/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route("/api/message")
-def home():
+def get_message():
     fake = Faker()
     msg = "This is a message from flask backend."
     return {"msg": fake.sentence()}
